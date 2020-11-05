ecfd8afb0c7c71c0e7f6cc99329e0750e02eec2e
Jina <yongjinc@bu.edu>
2020-11-05 01:43:57 -0500

    jina: added fetch

--- prototype/as3/src/App.js
+++ prototype/as3/src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            error:null,
+            isLoaded:false,
+            items:null
+        };
+    }
+
+    componentDidMount() {
+        //simple GET request using fetch
+        fetch('https://api.npms.io/v2/search?q=react')
+            .then(res => res.json())
+            .then(data => this.setState({
+                isLoaded: true,
+                items: data.total
+            }))
+    }
+
+    render() {
+        const {items, isLoaded, error} = this.state;
+        return (
+            <div>
+                Total packages: {items}
+            </div>
+        );
+    }
+
 }
 
 export default App;
